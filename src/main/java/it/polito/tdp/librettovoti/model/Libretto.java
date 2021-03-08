3ecf9dba57f88f1d1773867f83aafa2cef8245c8
Lorenzo <Lorenzo@LAPTOP-J00VVR3T>
2021-03-08 11:20:00 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -26,8 +26,41 @@ public class Libretto {
 		return s;
 		
 	}
-
+	/*
+	public void stampaVotiUguali(int punteggio) {
+		//Il libretto stampa da solo i voti
+	}
 	
+	public String votiUguali(int punteggio) {
+		//calcola una string che contine i voti
+		//sarà poi il chiamante a stamparli
+		return null;
+	}
+*/
+	public List<Voto> listaVotiUguali(int punteggio){
+		//però cosi espongo all'esterno la struttura dati usata internamente
+		List<Voto> risultato=new ArrayList<>();
+		for(Voto v : this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		//lo stesso oggetto in due liste diverse
+		//se non scrivo new, non ho nuovi oggetti
+		return risultato;
+		
+	}
+	
+	//preferisco dare un libretto allora
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato= new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
 	
 	
 }
